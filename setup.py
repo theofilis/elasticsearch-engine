ae3097f05c75fae8ade153edea97dbada1a58f73
George Theofilis <theofilis.g@gmail.com>
2015-06-19 22:20:34 +0300

    Finish with setup

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from distutils.core import setup
 
 setup(
     name='elasticsearch-engine',
-    version='v0.0.0',
+    version='v0.0.1',
     packages=['elasticsearch_engine'],
     url='https://github.com/theofilis/elasticsearch-engine',
     license='GPLv2',
     author='theofilis',
     author_email='theofilis,g@gmail.com',
-    description=''
+    description='',
+    package_dir={'': 'src'},
+    packages=['elasticsearch_engine'],
 )
