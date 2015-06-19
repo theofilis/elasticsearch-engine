2fd59cf69bbeb6d150cf1ee648ed836d6772148f
George Theofilis <theofilis.g@gmail.com>
2015-06-19 22:13:41 +0300

    Merge branch 'master' of github.com:theofilis/elasticsearch-engine
    Conflicts:
    	.gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='elasticsearch-engine',
+    version='',
+    packages=['elasticsearch_engine'],
+    url='',
+    license='',
+    author='theofilis',
+    author_email='theofilis,g@gmail.com',
+    description=''
+)
